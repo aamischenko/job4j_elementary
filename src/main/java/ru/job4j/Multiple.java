a4314a218f6bf436e76e5e654a7355fdf63ff1be
aamischenko <aamischenko@mail.ru>
2023-02-17 17:24:41 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,5 +1,5 @@
 package ru.job4j;
-public class Multiple {
+public class Multiple{
     public static void main(String[] args){
         System.out.println("1 * 1 = 1");
         System.out.println("1 * 2 = 2");
